0f6deb04090c41fd8ad3823bbf6bd93a10d5f624
saaheilKalyani <saaheil0909@gmail.com>
2025-11-02 10:30:48 +0530

    Home Page

---
+++ src/main/java/com/medicalstore/controller/HomeController.java
@@ -0,0 +1,23 @@
+package com.medicalstore.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+
+    @GetMapping("/about")
+    public String about() {
+        return "about";
+    }
+
+    @GetMapping("/features")
+    public String features() {
+        return "features";
+    }
+}
